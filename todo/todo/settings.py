87675bca57349c4dbb46d2ca3e777f96affa8a88
Leonardo Santos <leonardodossantoss50@gmail.com>
2022-12-08 18:00:38 -0300

    estilos bootstrap4 importados para formulários de addtask e edittask

--- todo/todo/settings.py
+++ todo/todo/settings.py
@@ -40,7 +40,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'tasks',
     'about',
-    'users'
+    'users',
+    'crispy_forms',
 ]
 
 MIDDLEWARE = [
@@ -126,6 +127,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
