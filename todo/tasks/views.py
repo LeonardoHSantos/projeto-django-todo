df09c1b60a3d813c32f694d378a1e161d44168b8
Leonardo Santos <leonardodossantoss50@gmail.com>
2022-12-08 20:36:33 -0300

    Implementado métodos de atualização do status da tarefa com o botão class change-status e estilos de acordo com o status da tarefa.

--- todo/tasks/views.py
+++ todo/tasks/views.py
@@ -74,6 +74,22 @@ def deleteTask(request, id):
 
     return redirect('/')
 
+
+@login_required
+def chengeStatus(request, id):
+    task = get_object_or_404(Task, pk=id)
+
+    if (task.done == 'doing'):
+        task.done = 'done'
+    else:
+        task.done = 'doing'
+    
+    task.save()
+
+    return redirect('/')
+
+
+
 @login_required
 def helloword(request):
     return HttpResponse("Hello World!")
